465b5fddd7156e7c8cf723d32bb0bdc16b7efab9
Sandip-Tiwari-435 <sandipt335@gmail.com>
2023-04-05 08:51:50 +0530

    Just checking 123

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,40 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Login from './pages/auth/Login';
-import PrivateRoute from './components/routes/PrivateRoute';
-import NotFound from './pages/NotFound';
-import { Toaster } from 'react-hot-toast';
-import Menu from './components/nav/menu';
-import UserDashboard from './pages/user/UserDashboard';
-import AdminDashboard from './pages/admin/AdminDashboard'
-import ViewUser from './pages/admin/ViewUser';
-import AdminRoute from './components/routes/AdminRoute';
-import CreateDrive from './pages/admin/CreateDrive';
-import RegisterStudent from './pages/admin/RegisterStudent';
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import PrivateRoute from "./components/routes/PrivateRoute";
+import NotFound from "./pages/NotFound";
+import { Toaster } from "react-hot-toast";
+import Menu from "./components/nav/menu";
+import UserDashboard from "./pages/user/UserDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import ViewUser from "./pages/admin/ViewUser";
+import AdminRoute from "./components/routes/AdminRoute";
+import CreateDrive from "./pages/admin/CreateDrive";
+import RegisterStudent from "./pages/admin/RegisterStudent";
 
-// import  
+// import
 
 function App() {
   return (
+    //checking 123 by sandip. once the pr is merged lemme know on whatsapp
     <BrowserRouter>
-    <Menu/>
-    <Toaster/>
+      <Menu />
+      <Toaster />
       <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/' element={<PrivateRoute/>}>
-          <Route path='' element={<Home/>}/>
-          <Route path='user/dashboard' element={<UserDashboard/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="" element={<Home />} />
+          <Route path="user/dashboard" element={<UserDashboard />} />
         </Route>
-        <Route path='/admin' element={<AdminRoute/>}>
-          <Route path='create-drive' element={<CreateDrive/>}/>
-          <Route path='register-student' element={<RegisterStudent/>}/>
-          <Route path='dashboard' element={<AdminDashboard/>}/>
-          <Route path='view-user/:userID' element={<ViewUser/>}/>
+        <Route path="/admin" element={<AdminRoute />}>
+          <Route path="create-drive" element={<CreateDrive />} />
+          <Route path="register-student" element={<RegisterStudent />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="view-user/:userID" element={<ViewUser />} />
         </Route>
-        <Route path='*' element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
